d4e1ffe53bf6b76b3975c229324c5e8a11cb4df4
docteurdev <77064953+docteurdev@users.noreply.github.com>
2023-01-02 15:59:02 +0100

    add sequelize param port ok

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -6,7 +6,7 @@ const PersonModel= require('../model/persone');
 
 const sequelize= new Sequelize(process.env.DB_NAME || 'testy',process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'root',{
     host:process.env.DB_HOST || 'localhost',
-    port:process.env.PORT || 3306,
+    port:process.env.DB_PORT || 3306,
     dialect: 'mysql',
     // dialectOptions:{
     //     timezone: 'Etc/GMT-2'
